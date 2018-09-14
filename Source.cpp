ba89d4f7292ac01d6fefba2fadeed820be7ae455
Artery <artemogloblin56@gmail.com>
2018-09-14 15:16:51 +0300

    first commit

---
+++ Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+struct msg {
+	char* currency_name1;
+	double currency_amount1;
+	char* currency_name2;
+	double currency_amount2;
+
+
+
+};
+
+int main() {
+
+	
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
